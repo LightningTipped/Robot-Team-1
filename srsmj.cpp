0ea9f9e9248f83e4b8f6aa2af4b24312cf6e5983
Reid <reidwicks@hotmail.com>
2017-05-04 14:56:10 +1200

    Initial upload
    It's a start, I guess

---
+++ srsmj.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+//look at assignment 5 to understand this
+
+int main(){
+	init();
+	
+	set_motor(1, 255);
+	sleep1(0,500000);
+	
+	set_motor(2, 255);
+	sleep1(0,500000);
+	
+	stop(1);
+	stop(2);
+	
+	return 0;
+}
