3826f7aad229ff006139e9c5cc57f12fd28f19b0
Reid <reidwicks@hotmail.com>
2017-05-08 11:44:04 +1200

    comments

--- srsmj.cpp
+++ srsmj.cpp
@@ -7,11 +7,11 @@
 int main(){
 	init();
 	
-	set_motor(1, 100);
-	sleep1(0,500000);
+	set_motor(1, 100); //tells motor 1 to go a bit
+	sleep1(0,500000); //sleeps
 	
-	set_motor(2, 100);
-	sleep1(0,500000);
+	set_motor(2, 100); //tells motor 2 to go a bit
+	sleep1(0,500000); //sleeps
 	
 	stop(1);
 	stop(2);
