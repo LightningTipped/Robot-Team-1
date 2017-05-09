00ab99b4633ea32c92edfd7173c72411f29d9f39
Reid <reidwicks@hotmail.com>
2017-05-09 14:52:25 +1200

    this version is an if-statement. Next version will be a for loop (I think)

--- srsmj.cpp
+++ srsmj.cpp
@@ -12,31 +12,31 @@ int main(){
 	init();
 	moveForward();
 	
-	
 	return 0;
 }
 
 int moveForward(){
-	take_picture();
-	int my_pixel = get_pixel(160, 120, 3);
-	if(my_pixel >= 127){
-		set_motor(1, 20); //tells motor 1 to go a bit
-		sleep1(5,0); //sleeps
-		
-		set_motor(2, 20); //tells motor 2 to go a bit
-		sleep1(5,0); //sleeps
-	}
+	while(true){
+		take_picture();
+		int my_pixel = get_pixel(160, 120, 3);
+		printf("%d\n", my_pixel);
+		if(my_pixel >= 127){
+			set_motor(1, 50); //tells motor 1 to go a bit
+			set_motor(2, 50); //tells motor 2 to go a bit
+			sleep1(0,1000); //sleeps
+		}
+	}	
 	stop(1);
 	stop(2);
 	
 	return 0;
 }
 int moveBackward(){
-	set_motor(1, -20);
-	sleep1(5,0);
+	set_motor(1, -50);
+	sleep1(0,1000); //sleeps
 	
-	set_motor(2, -20);
-	sleep1(5,0);
+	set_motor(2, -50);
+	sleep1(0,1000); //sleeps
 	
 	stop(1);
 	stop(2);
