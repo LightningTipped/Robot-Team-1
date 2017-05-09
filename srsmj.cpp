06c74ea25104eebadb2429c96991051693590299
Reid <reidwicks@hotmail.com>
2017-05-09 14:22:32 +1200

    making progress!

--- srsmj.cpp
+++ srsmj.cpp
@@ -6,15 +6,32 @@
 
 int main(){
 	init();
+	moveForward();
 	
-	set_motor(1, 100); //tells motor 1 to go a bit
-	sleep1(0,500000); //sleeps
 	
-	set_motor(2, 100); //tells motor 2 to go a bit
-	sleep1(0,500000); //sleeps
+	return 0;
+}
+
+int moveForward(){
+	take_picture();
+	my_pixel = get_pixel(160, 120, 3);
+	if(my_pixel > 160){
+		set_motor(1, 20); //tells motor 1 to go a bit
+		sleep1(5,0); //sleeps
+		
+		set_motor(2, 20); //tells motor 2 to go a bit
+		sleep1(5,0); //sleeps
+	}
+	stop(1);
+	stop(2);
+}
+int moveBackward(){
+	set_motor(1, -20);
+	sleep1(5,0);
+	
+	set_motor(2, -20);
+	sleep1(5,0);
 	
 	stop(1);
 	stop(2);
-	
-	return 0;
 }
