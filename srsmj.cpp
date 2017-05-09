5092acf919c71dcdac7527532f7f4d27fd14ea72
Reid <reidwicks@hotmail.com>
2017-05-09 23:49:09 +1200

    I hope I did this right...

--- srsmj.cpp
+++ srsmj.cpp
@@ -2,29 +2,36 @@
 #include <time.h>
 #include "E101.h"
 
-//look at assignment 5 to understand this
-
 /* motor 1 = left hand side
  * motor 2 = right hand side 
 /**/
 
-int main(){
-	init();
-	moveForward();
-	
-	return 0;
-}
-
 int moveForward(){
+	
+	int lSpeed = 50;
+	int rSpeed = 50;
+	int scale = 0.2;
+	int total = 0;
+	bool isWhite = false;
+	int count = 0;
+	
 	while(true){
+		
+		count = 0;
+		total = 0;
 		take_picture();
-		int my_pixel = get_pixel(160, 120, 3);
-		printf("%d\n", my_pixel);
-		if(my_pixel >= 127){
-			set_motor(1, 50); //tells motor 1 to go a bit
-			set_motor(2, 50); //tells motor 2 to go a bit
-			sleep1(0,1000); //sleeps
+		for(int i=0, i<319, i++){
+			isWhite = (get_pixel(120, i, 3) > 100);
+			if(isWhite){
+				total += (160-i);
+				count ++;
 		}
+		
+		printf("count: %d\n", count);
+		printf("result: %d\n", total);
+		
+		set_motor(1, lSpeed + total*scale);
+		set_motor(2, rSpeed + total*scale);
 	}	
 	stop(1);
 	stop(2);
@@ -32,14 +39,18 @@ int moveForward(){
 	return 0;
 }
 int moveBackward(){
-	set_motor(1, -50);
-	sleep1(0,1000); //sleeps
-	
-	set_motor(2, -50);
-	sleep1(0,1000); //sleeps
+	set_motor(1, lSpeed - total*scale);
+	set_motor(2, rSpeed - total*scale);
 	
 	stop(1);
 	stop(2);
+	
+	return 0;
+}
+
+int main(){
+	init();
+	moveForward();
 	
 	return 0;
 }
